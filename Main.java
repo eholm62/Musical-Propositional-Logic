7a677a449a35a1138c3c8a044d7370ea80f1d8ed
Elliot Holmes <133877996+eholm62@users.noreply.github.com>
2024-05-01 17:24:12 +0000

    added a parameter to main such that you can specify if you would not like it to play audio

--- Main.java
+++ Main.java
@@ -8,18 +8,28 @@ import stdlib.StdAudio;
 class Main {
 	private static int i;
 	private static boolean[] truthData;
+	private static boolean useAudio;
 	
 	public static void main(String[] args) throws InterruptedException {
 		final Proposition proposition = new Proposition(args[0]);
 		final long beatLength = Long.parseLong(args[1]);
 		
+		if (args.length > 2) {
+			useAudio = Boolean.parseBoolean(args[2]);
+		} else {
+			useAudio = true;
+		}
+
 		i = 0;
 		truthData = Interpreter.makeTruthTable(proposition);
 		System.out.println(Arrays.toString(truthData));
 		
+
 		final Runnable playBeat = () -> {
 			if (truthData[i]) {
-				// StdAudio.play("click.wav");
+				if (useAudio) {
+					StdAudio.play("click.wav"); 
+				}
 				System.out.println("click");
 			} else {
 				System.out.println(".");
